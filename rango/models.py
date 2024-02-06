250a9cbc89af455a32b8cfef52529aff8983e75a
ZhenYao Heng <2840506h@student.gla.ac.uk>
2024-02-06 16:02:10 +0000

    C9 final

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 # Create your models here.
@@ -32,3 +33,14 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = 'rango:login'
